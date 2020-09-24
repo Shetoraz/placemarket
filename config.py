d4e1e52fc4ece0f7ca1f9599fba96c81ed74ef59
Anton <grizzliboom@gmail.com>
2020-09-24 22:57:18 +0300

    Removed shit info

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from enum import Enum
 token = "1162949489:AAE4Q8DLG_s_Kgz8xfrrWOhRRDyJBRiQ-T0"
 admin_chat_id = 1266099533
 name = "marketplaceby"
-db_file = "database.vdb"
 DATABASELINK = "postgres://usupsjjfwbzaex:2f6588fb27317944ea45665c5db330af993b86b029ef1fb3b297c85ad7055e91@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/dcg3c38m5qoi9t"
 remote_name = "announcement" 
 
